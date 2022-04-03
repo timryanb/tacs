76cade7b159668833b57127598106f922ffdd051
trbrooks <tim.r.brooks@nasa.gov>
2022-04-03 10:17:35 -0400

    minor edit

--- tests/integration_tests/pytacs_analysis_base_test.py
+++ tests/integration_tests/pytacs_analysis_base_test.py
@@ -2,6 +2,25 @@ import numpy as np
 from tacs import TACS
 import unittest
 from mpi4py import MPI
+import os
+import tacs
+import mpi4py
+
+tacs_dir = os.path.dirname(tacs.__file__)
+mpi4py_dir = os.path.dirname(mpi4py.__file__)
+
+tacs_files = os.listdir(tacs_dir)
+mpi4py_files = os.listdir(mpi4py_dir)
+
+print("TACS LIBRARIES...............................")
+for file in tacs_files:
+    if os.path.splitext(file)[-1] == '.so':
+        os.system(f"otool -L {tacs_dir}/{file}")
+
+print("MPI4PY LIBRARIES...............................")
+for file in mpi4py_files:
+    if os.path.splitext(file)[-1] == '.so':
+        os.system(f"otool -L {mpi4py_dir}/{file}")
 
 '''
 This is a base class for running pytacs unit test cases.
